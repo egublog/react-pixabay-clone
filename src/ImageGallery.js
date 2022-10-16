6e6c81751d56943b67d3fd83ea663587c74a8b14
egublog <egukei0729@gmail.com>
2022-10-16 16:57:05 +0900

    [ADD]仮画像

--- src/ImageGallery.js
+++ src/ImageGallery.js
@@ -1,8 +1,15 @@
 import React from 'react'
+import "./ImageGallery.css";
 
 const ImageGallery = () => {
   return (
-    <div>ImageGallery</div>
+    <div>
+      <div className="images-wrapper">
+        <div className="image">
+          <img src="https://pixabay.com/get/g7126c0b5c33fccb9b386be238552788ee0cf4772479b81a0b9dd0bfde69053eaaa56cd9f50d3a719e6b22c199ba98c163cbddadbe28daf24f521f033ac7d7633_1280.jpg" alt="" />
+        </div>
+      </div>
+    </div>
   )
 }
 
