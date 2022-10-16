5767c53b34d82531dde2f674770f9d2224b62053
egublog <egukei0729@gmail.com>
2022-10-16 16:20:00 +0900

    [MODIFY]タイトルとform

--- src/App.js
+++ src/App.js
@@ -3,21 +3,11 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h2>My Pixabay</h2>
+      <from>
+      <input type="text" placeholder="画像を探す" />
+      </from>
     </div>
   );
 }
