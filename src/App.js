744fc5f8a5f260afad0d3a73385615a6ddf339b7
egublog <egukei0729@gmail.com>
2022-10-16 17:10:07 +0900

    [ADD]inputの中身を取得

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
+import { useRef, useState } from 'react';
 import './App.css';
 import ImageGallery from './ImageGallery';
 
 function App() {
+  const [inputText, setInputText] = useState("");
+  const ref = useRef();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(ref.current.value);
+  };
+
   return (
     <div className="container">
       <h2>My Pixabay</h2>
-      <from>
-      <input type="text" placeholder="画像を探す" />
+      <from onSubmit={(e) => handleSubmit(e)}>
+        <input type="text" placeholder="画像を探す" ref={ref} />
       </from>
       <ImageGallery />
     </div>
