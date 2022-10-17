a6530c58cddd06e1ec447ab36b969cb8200eab64
egublog <egukei0729@gmail.com>
2022-10-17 23:57:27 +0900

    [ADD]APIのfetch

--- src/App.js
+++ src/App.js
@@ -9,6 +9,22 @@ function App() {
   const handleSubmit = (e) => {
     e.preventDefault();
     console.log(ref.current.value);
+
+    const API_KEY = '30631304-504e1a4f405e428c58b536e1a';
+    // APIのURL
+    const endpointURL = 'https://pixabay.com/api/?key=' + API_KEY + '&q=' + ref.current.value + '&image_type=photo';
+
+    // APIを叩く(データフェッチング)
+    fetch(endpointURL)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        setInputText(data.hits);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+
   };
 
   return (
