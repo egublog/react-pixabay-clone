d619f4ee0f12f7f0b3ae53b737b27eabd1faeea2
egublog <egukei0729@gmail.com>
2022-10-18 23:57:30 +0900

    [MODIFY]fetchData

--- src/ImageGallery.js
+++ src/ImageGallery.js
@@ -1,31 +1,18 @@
-import React from 'react'
+import React from "react";
 import "./ImageGallery.css";
 
-const ImageGallery = () => {
+const ImageGallery = ({ fetchData }) => {
   return (
     <div>
       <div className="images-wrapper">
-        <div className="image">
-          <img src="https://pixabay.com/get/g7126c0b5c33fccb9b386be238552788ee0cf4772479b81a0b9dd0bfde69053eaaa56cd9f50d3a719e6b22c199ba98c163cbddadbe28daf24f521f033ac7d7633_1280.jpg" alt="" />
-        </div>
-        <div className="image">
-          <img src="https://pixabay.com/get/g7126c0b5c33fccb9b386be238552788ee0cf4772479b81a0b9dd0bfde69053eaaa56cd9f50d3a719e6b22c199ba98c163cbddadbe28daf24f521f033ac7d7633_1280.jpg" alt="" />
-        </div>
-        <div className="image">
-          <img src="https://pixabay.com/get/g7126c0b5c33fccb9b386be238552788ee0cf4772479b81a0b9dd0bfde69053eaaa56cd9f50d3a719e6b22c199ba98c163cbddadbe28daf24f521f033ac7d7633_1280.jpg" alt="" />
-        </div>
-        <div className="image">
-          <img src="https://pixabay.com/get/g7126c0b5c33fccb9b386be238552788ee0cf4772479b81a0b9dd0bfde69053eaaa56cd9f50d3a719e6b22c199ba98c163cbddadbe28daf24f521f033ac7d7633_1280.jpg" alt="" />
-        </div>
-        <div className="image">
-          <img src="https://pixabay.com/get/g7126c0b5c33fccb9b386be238552788ee0cf4772479b81a0b9dd0bfde69053eaaa56cd9f50d3a719e6b22c199ba98c163cbddadbe28daf24f521f033ac7d7633_1280.jpg" alt="" />
-        </div>
-        <div className="image">
-          <img src="https://pixabay.com/get/g7126c0b5c33fccb9b386be238552788ee0cf4772479b81a0b9dd0bfde69053eaaa56cd9f50d3a719e6b22c199ba98c163cbddadbe28daf24f521f033ac7d7633_1280.jpg" alt="" />
-        </div>
+        {fetchData.map((data) => (
+          <div className="image">
+            <img src={data.largeImageURL} alt="" />
+          </div>
+        ))}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default ImageGallery
+export default ImageGallery;
